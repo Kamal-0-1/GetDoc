6254cc3eb777fbe296a68d6e2fcf235172b27737
Kamal <110046714+Kamal-0-1@users.noreply.github.com>
2023-11-16 13:23:43 +0530

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,38 @@
+<nav class="navbar navbar-expand-lg bg-light fixed-top shadow-lg">
+                <div class="container">
+                    <div class="collapse navbar-collapse " id="navbarNav">
+                        <ul class="navbar-nav m-auto">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="index.php">Home</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="#about">About</a>
+                            </li>
+
+                           
+
+                            <!-- <a class="navbar-brand d-none d-lg-block" href="index.php">
+                                Doctor Appointment
+                                <strong class="d-block">Management System</strong>
+                            </a> -->
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="check-appointment.php">Check Appointment</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="#booking">Booking</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="#contact">Customer Support</a>
+                            </li>
+                             <li class="nav-item active">
+                                <a class="nav-link" href="doctor/login.php">Doctor</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                </div>
+</nav>
