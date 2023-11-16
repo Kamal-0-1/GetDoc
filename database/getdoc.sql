c1e5d57ba1226cd0a8e5f45e63199dd85e714d28
Kamal <110046714+Kamal-0-1@users.noreply.github.com>
2023-11-16 13:24:34 +0530

    Add files via upload

---
+++ database/getdoc.sql
@@ -0,0 +1,143 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 16, 2023 at 08:36 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `getdoc`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblappointment`
+--
+
+CREATE TABLE `tblappointment` (
+  `ID` int(10) NOT NULL,
+  `AppointmentNumber` int(10) DEFAULT NULL,
+  `Name` varchar(250) DEFAULT NULL,
+  `MobileNumber` bigint(20) DEFAULT NULL,
+  `Email` varchar(250) DEFAULT NULL,
+  `AppointmentDate` date DEFAULT NULL,
+  `AppointmentTime` time DEFAULT NULL,
+  `Specialization` varchar(250) DEFAULT NULL,
+  `Doctor` int(10) DEFAULT NULL,
+  `Message` mediumtext DEFAULT NULL,
+  `ApplyDate` timestamp NULL DEFAULT current_timestamp(),
+  `Remark` varchar(250) DEFAULT NULL,
+  `Status` varchar(250) DEFAULT NULL,
+  `UpdationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbldoctor`
+--
+
+CREATE TABLE `tbldoctor` (
+  `ID` int(5) NOT NULL,
+  `FullName` varchar(250) DEFAULT NULL,
+  `Location` varchar(250) DEFAULT NULL,
+  `MobileNumber` bigint(10) DEFAULT NULL,
+  `Email` varchar(250) DEFAULT NULL,
+  `Specialization` varchar(250) DEFAULT NULL,
+  `Password` varchar(259) DEFAULT NULL,
+  `CreationDate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblspecialization`
+--
+
+CREATE TABLE `tblspecialization` (
+  `ID` int(5) NOT NULL,
+  `Specialization` varchar(250) DEFAULT NULL,
+  `CreationDate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblspecialization`
+--
+
+INSERT INTO `tblspecialization` (`ID`, `Specialization`, `CreationDate`) VALUES
+(1, 'Orthopedics', '2022-11-09 14:22:33'),
+(2, 'Internal Medicine', '2022-11-09 14:23:42'),
+(3, 'Obstetrics and Gynecology', '2022-11-09 14:24:14'),
+(4, 'Dermatology', '2022-11-09 14:24:42'),
+(5, 'Pediatrics', '2022-11-09 14:25:06'),
+(6, 'Radiology', '2022-11-09 14:25:31'),
+(7, 'General Surgery', '2022-11-09 14:25:52'),
+(8, 'Ophthalmology', '2022-11-09 14:27:18'),
+(9, 'Family Medicine', '2022-11-09 14:27:52'),
+(10, 'Chest Medicine', '2022-11-09 14:28:32'),
+(11, 'Anesthesia', '2022-11-09 14:29:12'),
+(12, 'Pathology', '2022-11-09 14:29:51'),
+(13, 'ENT', '2022-11-09 14:30:13'),
+(14, 'General', '2023-03-08 15:30:13');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tblappointment`
+--
+ALTER TABLE `tblappointment`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tbldoctor`
+--
+ALTER TABLE `tbldoctor`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tblspecialization`
+--
+ALTER TABLE `tblspecialization`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tblappointment`
+--
+ALTER TABLE `tblappointment`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+--
+-- AUTO_INCREMENT for table `tbldoctor`
+--
+ALTER TABLE `tbldoctor`
+  MODIFY `ID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+--
+-- AUTO_INCREMENT for table `tblspecialization`
+--
+ALTER TABLE `tblspecialization`
+  MODIFY `ID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
